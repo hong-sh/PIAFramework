58ecad419fd6291065ccc364d9029b178d319acc
hong-sh <rkskekfk2020@naver.com>
2022-02-20 14:05:16 +0900

    base agent class commit

---
+++ agent/agent.py
@@ -0,0 +1,44 @@
+from abc import *
+from enum import Enum
+
+class AgentStatus(Enum):
+    CREATED = 0
+    RUNNING = 1
+    DESTROYED = 2
+    FAIL = 3
+
+class BaseAgent(metaclass=ABCMeta):
+    def __init__(self, blackboard_url:str, agent_uri:str):
+        self.blackboard_url = blackboard_url
+        self.agent_uri = agent_uri
+        self.agent_status = AgentStatus.CREATED
+
+    @abstractmethod
+    def on_start(self):
+        pass
+    
+    @abstractmethod
+    def on_stop(self):
+        pass
+
+    @abstractmethod
+    def on_subscribe(self, sender:str, message:str):
+        pass
+
+    @abstractmethod
+    def on_unsubscribe(self, sender:str, message:str):
+        pass
+
+    @abstractmethod
+    def on_request(self, sendder:str, message:str):
+        pass
+
+    @abstractmethod
+    def on_response(self, sender:str, message:str):
+        pass
+
+    def publish(self, receiver:str, message:str):
+        pass
+
+    def request(self, receiver:str, message:str):
+        pass
