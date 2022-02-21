0394e58bbe2709df35c352f21d84a736b0fd734d
hong-sh <rkskekfk2020@naver.com>
2022-02-21 17:54:16 +0900

    blackboard commit

--- framework/blackboard.py
+++ framework/blackboard.py
@@ -1,7 +1,18 @@
 import redis
 
 class BlackBoard:
-    self.redis_server = None
+    def __init__(self, blackboard_url:str):
+        try:
+            splitted_url = blackboard_url.split(":")
+            host, port = splitted_url[0], splitted_url[1]
+            self.redis_cli = redis.Redis(host=host, port=port, db=0)
+            self.subscribe = self.redis_cli.pubsub()
+        except Exception as e:
+            print('redis connect error occurred : ', e)
 
-    def __init__(self):
+    def req_publish(self, publisher:str, message:str):
+        
+        self.redis_cli.publish()
+
+    def req_subscribe(self, subscriber:str, receiver:str, message:str):
         pass
