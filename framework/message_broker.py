f46410886adf26064fe10853be41f4e621bc6ba5
hong-sh <rkskekfk2020@naver.com>
2022-02-23 23:21:29 +0900

    message broker commit

--- framework/message_broker.py
+++ framework/message_broker.py
@@ -1,7 +1,46 @@
+from concurrent import futures
 import messaging_service.PIA_message_pb2_grpc as PIA_message_grpc
 import messaging_service.PIA_message_pb2 as PIA_message
 import blackboard
+import grpc
+from threading import Thread
 
 class MessageBroker(PIA_message_grpc.PIAMessageServiceServicer):
     def __init__(self, broker_url:str, blackboard:blackboard):
+        self.broker_url = broker_url
+        self.black_board = blackboard
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        PIA_message_grpc.add_PIAMessageServiceServicer_to_server(PIAMessageService(self), self.server)
+        self.server.add_insecure_port(self.broker_url)
+        self.server.start()
+        thread = Thread(target=self.server.wait_for_termination())
+        thread.start()
+
+    def on_publish(self, ):
         pass
+
+    def on_subscribe(self, ):
+        pass
+
+    def on_unsubscribe(self,):
+        pass
+
+    def on_request(self,):
+        pass
+
+        
+class PIAMessageService(PIA_message_grpc.PIAMessageServiceServicer):
+    def __init__(self, message_broker:MessageBroker):
+        self.message_broker = message_broker
+
+    def Publish(self, request, context):
+        return super().Publish(request, context)
+
+    def Subscribe(self, request, context):
+        return super().Subscribe(request, context)
+
+    def UnSubscribe(self, request, context):
+        return super().UnSubscribe(request, context)
+
+    def Request(self, request, context):
+        return super().Request(request, context)
