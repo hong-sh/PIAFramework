651cc089916dc33280d1dc39ce4d9ae03064e977
hong-sh <rkskekfk2020@naver.com>
2022-02-23 23:30:35 +0900

    message broker update

--- framework/message_broker.py
+++ framework/message_broker.py
@@ -16,24 +16,21 @@ class MessageBroker(PIA_message_grpc.PIAMessageServiceServicer):
         thread = Thread(target=self.server.wait_for_termination())
         thread.start()
 
-    def generate_key(self, ):
-        pass
-
-    def on_publish(self, publisher:str, message:PIA_message.PublishMessage):
-        # TODO generate message_key
+    def on_publish(self, message:PIA_message.PublishMessage):
         # TODO unmarshal message
         blackboard.req_publish()
         pass
 
-    def on_subscribe(self, ):
+    def on_subscribe(self, message:PIA_message.SubscribeMessage):
         blackboard.req_subscribe()
         pass
 
-    def on_unsubscribe(self,):
+    def on_unsubscribe(self, message:PIA_message.SubscribeMessage):
         blackboard.req_unsubscribe()
         pass
 
-    def on_request(self,):
+    def on_request(self, message:PIA_message.RequestMessage):
+        # TODO interaction directly request
         pass
 
         
@@ -42,13 +39,17 @@ class PIAMessageService(PIA_message_grpc.PIAMessageServiceServicer):
         self.message_broker = message_broker
 
     def Publish(self, request, context):
+        self.message_broker.on_publish(request)
         return super().Publish(request, context)
 
     def Subscribe(self, request, context):
+        self.message_broker.on_subscribe(request)
         return super().Subscribe(request, context)
 
     def UnSubscribe(self, request, context):
+        self.message_broker.on_unsubscribe(request)
         return super().UnSubscribe(request, context)
 
     def Request(self, request, context):
+        self.message_broker.on_request(request)
         return super().Request(request, context)
