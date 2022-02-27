c2fb1d79c9d45af98c4c5db9cd0465925db1ab33
hong-sh <rkskekfk2020@naver.com>
2022-02-27 18:26:42 +0900

    blackboard client update

--- agent/blackboard_cli.py
+++ agent/blackboard_cli.py
@@ -1,7 +1,6 @@
 import redis
 from threading import Thread
 
-class AgentMessage:
 
 class BlackBoardClient:
     def __init__(self, agent_uri:str, blackboard_url:str, callbacks:object):
@@ -19,22 +18,32 @@ class BlackBoardClient:
             print('redis connect error occurred : ', e)
 
     def initialize_topic(self):
-        self.subscribe.psubscribe(self.agent_uri)
+        self.subscribe.psubscribe(self.agent_uri+"/request/*")
+        self.subscribe.psubscribe(self.agent_uri+"/response/*")
 
     def on_subscribe(self):
         while True:
             message = self.subscribe.get_message()
-            self.subscribe.
+            if message['type'] == 'message' or message['type'] == 'pmessage':
+                agent_uri, message_type, message_key = message['channel'].split("/")
+                if message_type in self.callbacks:
+                    self.callbacks[message_type](agent_uri, message_key, message['data'])
 
     def req_publish(self, key:str, value:dict):
         pub_key = self.agent_uri + '/publish/' + key
         self.redis_cli.publish(pub_key, value)
         pass
 
-    def req_subscribe(self, subscriber:str, receiver:str, subscribe_pattern:str):
-        self.redis_cli.pubsub()
-        pass
+    def req_subscribe(self, publisher:str, is_pattern:bool, key:str):
+        subscribe_key = publisher + "/publish/" + key
+        if is_pattern:
+            self.subscribe.psubscribe(subscribe_key)
+        else:
+            self.subscribe.subscribe(subscribe_key)
 
-    def req_unsubscribe(self, subscirber:str, receiver:str, unsubscribe_pattern:str):
-        self.redis_cli.pubsub()
-        pass
+    def req_unsubscribe(self, publisher:str, is_pattern:bool, key:str):
+        unubscribe_key = publisher + "/publish/" + key
+        if is_pattern:
+            self.subscribe.punsubscribe(unubscribe_key)
+        else:
+            self.subscribe.unsubscribe(unubscribe_key)
