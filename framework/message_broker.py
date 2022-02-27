a6d668dc8f43e5a602b833c39a4bfee394381224
hong-sh <rkskekfk2020@naver.com>
2022-02-27 16:09:02 +0900

    message broker update

--- framework/message_broker.py
+++ framework/message_broker.py
@@ -1,14 +1,15 @@
 from concurrent import futures
-import messaging_service.PIA_message_pb2_grpc as PIA_message_grpc
-import messaging_service.PIA_message_pb2 as PIA_message
-import blackboard
+import framework.messaging_service.PIA_message_pb2_grpc as PIA_message_grpc
+import framework.messaging_service.PIA_message_pb2 as PIA_message
+import framework.blackboard
 import grpc
+from google.protobuf.empty_pb2 import Empty
 from threading import Thread
 
 class MessageBroker(PIA_message_grpc.PIAMessageServiceServicer):
-    def __init__(self, broker_url:str, blackboard:blackboard):
+    def __init__(self, broker_url:str, blackboard:framework.blackboard):
         self.broker_url = broker_url
-        self.black_board = blackboard
+        self.blackboard = blackboard
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         PIA_message_grpc.add_PIAMessageServiceServicer_to_server(PIAMessageService(self), self.server)
         self.server.add_insecure_port(self.broker_url)
@@ -17,39 +18,61 @@ class MessageBroker(PIA_message_grpc.PIAMessageServiceServicer):
         thread.start()
 
     def on_publish(self, message:PIA_message.PublishMessage):
-        # TODO unmarshal message
-        blackboard.req_publish()
-        pass
+        message_id = message.message_id
+        publisher = message.publisher
+        receiver = message.receiver
+        publish_key = message.publish_key
+        publish_value = message.publish_value
+        self.blackboard.req_publish(message_id, publisher, receiver, publish_key, publish_value)
+        
 
     def on_subscribe(self, message:PIA_message.SubscribeMessage):
-        blackboard.req_subscribe()
-        pass
+        message_id = message.message_id
+        subscriber = message.subscriber
+        receiver = message.receiver
+        subscribe_pattern = message.subscribe_pattern
+        result = self.blackboard.req_subscribe(message_id, subscriber, receiver, subscribe_pattern)
+        response = PIA_message.ResponseSubscribe()
+        response.message_id = result["message_id"]
+        response.response_status = result["response_status"]
+        response.subscribe_message = message
+        return response
+
 
     def on_unsubscribe(self, message:PIA_message.SubscribeMessage):
-        blackboard.req_unsubscribe()
-        pass
+        message_id = message.message_id
+        subscriber = message.subscriber
+        receiver = message.receiver
+        unsubscribe_pattern = message.subscribe_pattern
+        result = self.blackboard.req_unsubscribe(message_id, subscriber, receiver, unsubscribe_pattern)
+        response = PIA_message.ResponseSubscribe()
+        response.message_id = result["message_id"]
+        response.response_status = result["response_status"]
+        response.subscribe_message = message
+        return response
 
     def on_request(self, message:PIA_message.RequestMessage):
-        # TODO interaction directly request
-        pass
+        message_id = message.message_id
+        requester = message.requester
+        receiver = message.receiver
+        request_key = message.request_key
+        request_value = message.request_value
+        self.blackboard.req_request(message_id, requester, receiver, request_key, request_value)
 
         
 class PIAMessageService(PIA_message_grpc.PIAMessageServiceServicer):
     def __init__(self, message_broker:MessageBroker):
         self.message_broker = message_broker
+        return Empty()
 
     def Publish(self, request, context):
         self.message_broker.on_publish(request)
-        return super().Publish(request, context)
 
     def Subscribe(self, request, context):
-        self.message_broker.on_subscribe(request)
-        return super().Subscribe(request, context)
+        return self.message_broker.on_subscribe(request)
 
     def UnSubscribe(self, request, context):
-        self.message_broker.on_unsubscribe(request)
-        return super().UnSubscribe(request, context)
+        return self.message_broker.on_unsubscribe(request)
 
     def Request(self, request, context):
-        self.message_broker.on_request(request)
-        return super().Request(request, context)
+        return self.message_broker.on_request(request)
