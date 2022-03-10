13f2a1d90052d0acf03f698e033acc89cc4ff0c2
hong-sh <rkskekfk2020@naver.com>
2022-03-10 22:03:43 +0900

    framework main commit

--- framework/blackboard.py
+++ framework/blackboard.py
@@ -8,6 +8,7 @@ class BlackBoard:
     def run_redis_server(self):
         running_os = platform.system()
         if running_os == "Windows":
+            os.system("C:\\Program Files\\Redis\\redis-server --service-stop")
             os.system("C:\\Program Files\\Redis\\redis-server --service-start")
         elif running_os == "Darwin" or running_os == "Linux":
             os.system("sudo systemctl restart redis.service")
---
+++ framework/framework.py
@@ -0,0 +1,4 @@
+from blackboard import BlackBoard
+
+if __name__ == "__main__":
+    blackboard = BlackBoard()
