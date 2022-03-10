aaf1645357ee9952d77f312507125b7ff5dc6be6
hong-sh <rkskekfk2020@naver.com>
2022-03-10 21:59:40 +0900

    blackboard server run commit

--- framework/blackboard.py
+++ framework/blackboard.py
@@ -1,7 +1,15 @@
-import redis
+import platform
+import os
 
 class BlackBoard:
-    def __init__(self, blackboard_url:str):
-        
-        # TODO check redis server running and server open
-        pass
+    def __init__(self):
+        self.run_redis_server()
+
+    def run_redis_server(self):
+        running_os = platform.system()
+        if running_os == "Windows":
+            os.system("C:\\Program Files\\Redis\\redis-server --service-start")
+        elif running_os == "Darwin" or running_os == "Linux":
+            os.system("sudo systemctl restart redis.service")
+
+    
